9d3c10f8039a9cb47532bd8e4472f6c5f276ee98
noprom <tyee.noprom@qq.com>
2016-01-20 06:49:47 +0800

    add workspace code

---
+++ code1-2.R
@@ -0,0 +1,12 @@
+setwd("/Users/noprom/Documents/Dev/R/Pro/R in Action")
+options()
+options(digits = 3)
+# 创建一个包含20个均匀分布随机变量的向量
+x <- runif(3)
+# 摘要统计量
+summary(x)
+# 直方图
+hist(x)
+# 命令的历史记录保存到文 件.Rhistory中,工作空间(包含向量x)保存到文件.RData中
+savehistory()
+save.image()
