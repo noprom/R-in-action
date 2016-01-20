e99b40bd9f02b891a8637fbca3484c8668cb2f50
noprom <tyee.noprom@qq.com>
2016-01-20 23:44:41 +0800

    add matrix

---
+++ ch2 dataset/code2-1.R
@@ -0,0 +1,7 @@
+y <- matrix(1:20, nrow = 5, ncol = 4)
+y
+cells <- c(1, 26, 24, 68)
+rnames <- c("R1", "R2")
+cnames <- c("C1", "C2")
+mymatrix <- matrix(cells, nrow = 2, ncol = 2, byrow = TRUE, dimnames = list(rnames, cnames))
+mymatrix
