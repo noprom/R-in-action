a333cc9f7a00d5230ee522e7e01be7668784532a
noprom <tyee.noprom@qq.com>
2016-03-01 10:25:34 +0800

    3.4.2 Axes

--- ch3 graphs/code3-4.R
+++ ch3 graphs/code3-4.R
@@ -11,3 +11,32 @@ title(main="My Title", col.main="red",
       sub="My Subtitle", col.sub="blue",
       xlab="My X label", ylab="My Y label",
       col.lab="green", cex.lab=0.75)
+
+# 3.4.2 Axes
+# axis(side, at=, labels=, pos=, lty=, col=, las=, tck=, ...)
+# custom axes
+
+# Specifies data 
+x <- c(1:10)
+y <- x
+z <- 10/x
+opar <- par(no.readonly=TRUE)
+# Increases margins
+par(mar=c(5, 4, 4, 8) + 0.1)
+# Plots x vs. y, suppressing annotations
+plot(x, y, type="b",
+     pch=21, col="red",
+     yaxt="n", lty=3, ann=FALSE)
+
+# Adds an x versus 1/x line
+lines(x, z, type="b", pch=22, col="blue", lty=2)
+# Draws the axes
+axis(2, at=x, labels=x, col.axis="red", las=2)
+axis(4, at=z, labels=round(z, digits=2),
+     col.axis="blue", las=2, cex.axis=0.7, tck=-.01)
+mtext("y=1/x", side=4, line=3, cex.lab=1, las=2, col="blue")
+# Adds titles and text
+title("An Example of Creative Axes",
+      xlab="X values",
+      ylab="Y=X")
+par(opar)
