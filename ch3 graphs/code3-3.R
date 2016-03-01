7d821d05a8543505ac5882da74e35e1e1e5aaf52
noprom <tyee.noprom@qq.com>
2016-03-01 09:07:26 +0800

    plot colors

--- ch3 graphs/code3-3.R
+++ ch3 graphs/code3-3.R
@@ -7,5 +7,22 @@ par(opar)
 # 也可以将参数的值直接放到括号中
 plot(dose, drugA, type="b", lty=2, pch=17)
 
-# 参数演示:Symbols and lines
+# Symbols and lines
 plot(dose, drugA, type="b", lty=3, lwd=3, pch=15, cex=2)
+
+# Colors
+install.packages("RColorBrewer")
+
+library(RColorBrewer)
+# case 1:
+n <- 7
+mycolors <- brewer.pal(n, "Set1")
+barplot(rep(1,n), col=mycolors)
+display.brewer.all()
+
+# case 2:
+n <- 10
+mycolors <- rainbow(n)
+pie(rep(1, n), labels=mycolors, col=mycolors)
+mygrays <- gray(0:n/n)
+pie(rep(1, n), labels=mygrays, col=mygrays)
