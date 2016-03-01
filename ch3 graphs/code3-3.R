00b23213b5942c4a1a4e2d6736c97b1d5a723366
noprom <tyee.noprom@qq.com>
2016-03-01 09:26:27 +0800

    sumarize the plot

--- ch3 graphs/code3-3.R
+++ ch3 graphs/code3-3.R
@@ -28,3 +28,19 @@ mygrays <- gray(0:n/n)
 pie(rep(1, n), labels=mygrays, col=mygrays)
 
 # 3.3.3 Text characteristics
+par(font.lab=3, cex.lab=1.5, font.main=4, cex.main=2)
+
+# 3.3.4 Graph and margin dimensions
+par(pin=c(4,3), mai=c(1,.5, 1, .2))
+
+# To sum up
+dose  <- c(20, 30, 40, 45, 60)
+drugA <- c(16, 20, 27, 40, 60)
+drugB <- c(15, 18, 25, 31, 40)
+opar <- par(no.readonly=TRUE)
+par(pin=c(2, 3))
+par(lwd=2, cex=1.5)
+par(cex.axis=.75, font.axis=3)
+plot(dose, drugA, type="b", pch=19, lty=2, col="red")
+plot(dose, drugB, type="b", pch=23, lty=6, col="blue", bg="green")
+par(opar)
