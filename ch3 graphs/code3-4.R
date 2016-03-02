277ced1b49b2218453374b75567e13bb8eab3a01
noprom <tyee.noprom@qq.com>
2016-03-02 08:08:04 +0800

    add text

--- ch3 graphs/code3-4.R
+++ ch3 graphs/code3-4.R
@@ -46,4 +46,30 @@ plot.new()
 abline(v=seq(1, 10, 2), lty=2, col="blue")
 
 # 3.4.4 Legend
+install.packages("Hmisc")
+dose  <- c(20, 30, 40, 45, 60)
+drugA <- c(16, 20, 27, 40, 60)
+drugB <- c(15, 18, 25, 31, 40)
+opar <- par(no.readonly=TRUE)
+par(lwd=2, cex=1.5, font.lab=2)
+plot(dose, drugA, type="b",pch=15, lty=1, col="red", ylim=c(0, 60),
+     main="Drug A vs. Drug B",
+     xlab="Drug Dosage", ylab="Drug Response")
+lines(dose, drugB, type="b",pch=17, lty=2, col="blue")
+abline(h=c(30), lwd=1.5, lty=2, col="gray")
+library(Hmisc)
+minor.tick(nx=3, ny=3, tick.ratio=0.5)
+legend("topleft", inset=.05, title="Drug Type", c("A","B"),
+       lty=c(1, 2), pch=c(15, 17), col=c("red", "blue"))
+par(opar)
 
+# 3.4.5 Text annotations
+attach(mtcars)
+plot(wt, mpg,
+     main="Mileage vs. Car Weight",
+     xlab="Weight", ylab="Mileage",
+     pch=18, col="blue")
+text(wt, mpg,
+     row.names(mtcars),
+     cex=0.6, pos=4, col="red")
+detach(mtcars)
